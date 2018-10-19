c10ee929013c26b824ca29d9e43460f00ef558bc
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-10-19 13:17:19 -0300

    boilerplate inicial do projeto

---
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/CarteiraPescaApplication.java
@@ -0,0 +1,12 @@
+package br.ufla.lemaf.ti.carteirapesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraPescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraPescaApplication.class, args);
+	}
+}
