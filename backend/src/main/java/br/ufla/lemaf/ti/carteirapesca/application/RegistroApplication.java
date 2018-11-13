048e311ffd137d685b560a9b420c733354ae1137
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-13 14:54:21 -0200

    Iniciando camada de aplicação de registro

---
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/application/RegistroApplication.java
@@ -0,0 +1,27 @@
+package br.ufla.lemaf.ti.carteirapesca.application;
+
+import br.ufla.lemaf.ti.carteirapesca.domain.model.Protocolo;
+import br.ufla.lemaf.ti.carteirapesca.interfaces.registro.web.RegistroResource;
+
+/**
+ * Serviço de Registro da camada de application.
+ * <p>
+ * Gerencia o registro de licenças, bem como suas renovações.
+ *
+ * @author Highlander Paiva
+ * @since 1.0
+ */
+public interface RegistroApplication {
+
+	/**
+	 * Verifica se o usuario pode criar uma nova licença,
+	 * e em casos positivos cria a mesma.
+	 * <p>
+	 * Caso o usuário não estiver cadastrado, o cadastra.
+	 *
+	 * @param resource Os dados de registro
+	 * @return O número do protocolo da licença
+	 */
+	Protocolo registrar(RegistroResource resource);
+
+}
