57410b1dd055348566a1a08aee47589dc11a2746
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-19 11:22:02 -0200

    Removendo arquivos não usados

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,29 +1,74 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    main-wrapper
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<script>
+  import MainWrapper from "./layouts/MainWrapper";
+
+  export default {
+    components: { MainWrapper }
+  };
+</script>
+
+<style lang="sass">
+  @import "../theme/tools/variables"
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600')
+
+  // base
+  html, body
+    height: 100vh
+    width: 100vw
+    overflow: hidden
+    background-color: $--cor-background
+    font-size: $--fonte-base
+    font-weight: 300
+    text-align: left
+
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    color: $--cor-texto-primario
+
+  // Cores
+  .alerta
+    background-color: $--cor-realce-vermelho
+
+  .sucesso
+    background-color: $--cor-realce-verde
+
+  .neutro
+    background-color: $--cor-realce-cinza
+
+  .atencao
+    background-color: $--cor-realce-amarelo
+
+  .info
+    background-color: $--cor-realce-azul
+
+  // Tipografia
+  h1, h2, h3, h4, h5, h6, i, p
+    margin: 0
+    padding: 0
+    font-weight: 400
+
+  h1
+    font-size: $--fonte-grande
+
+  h2
+    font-size: $--fonte-titulo
+
+  h3
+    font-size: $--fonte-botao
+
+  h4, p
+    font-size: $--fonte-base
+
+  h5, h6
+    font-size: $--fonte-pequena
+
+  .hide
+    display: none
+
+
 </style>
---
+++ frontend/src/components/layouts/MainWrapper.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+	#main-wrapper
+		el-container
+			el-header
+			el-main
+</template>
+
+<script>
+	export default {
+      name: "MainWrapper",
+	}
+</script>
