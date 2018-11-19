1a334816ee9ca9c67a53fc5d54f8ff1112a7e8b7
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-19 10:39:02 -0200

    Adicionando configurações e importando bibliotecas comuns ao Vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,25 @@
+// CORE Imports
 import Vue from "vue";
-import App from "./App.vue";
+import App from "./components/App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
+import ApiService from "./services/shared/ApiService";
+// 3th Imports
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/pt-br";
+import Vue2Filters from "vue2-filters";
+// CSS Imports
+import "mdi/scss/materialdesignicons.scss";
 
+// Vue Inits & Configs
+Vue.prototype.$bus = new Vue();
 Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
+Vue.use(Vue2Filters);
+ApiService.init();
 
+// Instância Vue
 new Vue({
   router,
   store,
