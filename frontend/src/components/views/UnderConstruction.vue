bdecbcd252bdd7ce8675a2a1f391d175f556ce81
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-21 10:36:03 -0200

    Componente view UnderConstruction

---
+++ frontend/src/components/views/UnderConstruction.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	#under-contruction
+		i.lnr.lnr-construction
+		h1 Página em desenvolvimento!
+</template>
+
+<style lang="sass">
+	#under-contruction
+		margin-top: 20%
+		display: flex
+		flex-direction: column
+		justify-content: center
+		align-items: center
+		color: #9E9E9E
+
+		i
+			font-size: 100px
+</style>
