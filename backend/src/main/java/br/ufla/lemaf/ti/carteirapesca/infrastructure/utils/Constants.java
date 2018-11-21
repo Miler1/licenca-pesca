e784967d09b13adbb52485a3b997bfe787fda5a4
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-21 15:06:55 -0200

    Removendo constantes não usadas

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/infrastructure/utils/Constants.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/infrastructure/utils/Constants.java
@@ -1,12 +1,15 @@
 package br.ufla.lemaf.ti.carteirapesca.infrastructure.utils;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * Constantes da Aplicação.
  *
  * @author Highlander Paiva
  * @since 1.0
  */
-public final class Constants {
+@UtilityClass
+public class Constants {
 
 	public static final Integer EMAIL = 1;
 
@@ -15,22 +18,5 @@ public final class Constants {
 	public static final Integer ENDERECO_CORRESPONDENCIA = 2;
 
 	public static final Integer ZONA_RURAL = 2;
-
-	public static final long FIRST_MILLISECONDS_DATE = 0L;
-
-	public static final String DATE_FORMAT = "(\\d{4})-(\\d{2})-(\\d{2})T((\\d{2}):(\\d{2}):(\\d{2}))\\.(\\d{3})Z";
-
-	public static final String DATE_CAR_FORMAT = "(\\d{2})/(\\d{2})/(\\d{4}) (\\d{2}):(\\d{2}):(\\d{2})";
-
-	public static final String TIME_FORMAT = "(\\d{2}):(\\d{2})";
-
-	public static final Integer TIME_LENGTH = 10;
-
-
-	/**
-	 * Para evitar instanciação.
-	 */
-	private Constants() {
-	}
 
 }
