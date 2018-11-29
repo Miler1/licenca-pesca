cecc7ccc9cf9e62570602550c9bc42479630a596
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-29 10:16:49 -0200

    #8 Mensagens para o componente [en / pt-br]

--- frontend/src/messages.js
+++ frontend/src/messages.js
@@ -3,7 +3,23 @@ const messages = {
     message: {
       registerTitle: "Register Fishing License",
       footer: "© " + new Date().getFullYear() + " - Fishing Licence System",
-      underConstruction: "Page under construction!"
+      underConstruction: "Page under construction!",
+      register: {
+        steps: {
+          identification: "Identification",
+          info: "Additional information",
+          summary: "Summary"
+        },
+        access: {
+          cpf: "CPF",
+          passport: "Passaport",
+          search: "CPF or passport number:",
+          placeholder: {
+            cpf: "Enter the CPF",
+            passport: "Enter the passport number"
+          }
+        }
+      }
     }
   },
 
@@ -12,7 +28,23 @@ const messages = {
       registerTitle: "Registrar Licença de Pesca",
       footer:
         "© " + new Date().getFullYear() + " - Sistema de Licença de Pesca",
-      underConstruction: "Página em desenvolvimento!"
+      underConstruction: "Página em desenvolvimento!",
+      register: {
+        steps: {
+          identification: "Identificação",
+          info: "Informações complementares",
+          summary: "Resumo"
+        },
+        access: {
+          cpf: "CPF",
+          passport: "Passaporte",
+          search: "CPF ou número do passaporte:",
+          placeholder: {
+            cpf: "Informe o CPF",
+            passport: "Informe o passaporte"
+          }
+        }
+      }
     }
   }
 };
