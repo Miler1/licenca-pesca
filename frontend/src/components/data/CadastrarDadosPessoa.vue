5d5f64b6745558bc1b365616c4fe209971ef604b
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-11-30 17:27:43 -0200

    #18 Model de Pessoa e Componente de cadastro inicial

---
+++ frontend/src/components/data/CadastrarDadosPessoa.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+	#cadastrar-dados-pessoa
+		el-form(:model="pessoa" ref="pessoa" :rules="pessoaRules" label-position="top")
+			h3 {{ $t("interface.registrar.identificacao.cadastro.titulos.dadosPessoais") }}
+
+			el-row.section(:gutter="20")
+
+				el-col(:span="6")
+					el-checkbox(v-model="pessoa.estrangeiro") {{ $t("interface.registrar.identificacao.cadastro.labels.estrangeiro") }}
+
+			el-row.section(:gutter="20")
+
+				el-col(:span="6")
+					el-form-item(:label="$t('interface.registrar.identificacao.cadastro.labels.nome')")
+						el-input(v-model="pessoa.nome")
+
+</template>
+
+<script>
+import { Pessoa } from "../../model/Pessoa";
+import { PESSOA_RULES } from "../../utils/validations/pessoaValidation";
+import ElInput from "../elements/InputElement";
+
+export default {
+  name: "CadastrarDadosPessoa",
+  components: { ElInput },
+  data() {
+    return {
+      pessoa: Pessoa,
+      pessoaRules: PESSOA_RULES
+    };
+  }
+};
+</script>
+
+<style lang="sass">
+	@import "../../theme/tools/variables"
+
+	#cadastrar-dados-pessoa
+		margin-top: 30px
+
+		.section
+			margin-top: 20px
+
+		.el-form-item__label
+			padding: 0 !important
+</style>
---
+++ frontend/src/model/Pessoa.js
@@ -0,0 +1,36 @@
+export const Pessoa = {
+  estrangeiro: null,
+  nome: null,
+  cpf: null,
+  passaporte: null,
+  dataNascimento: null,
+  sexo: null,
+  nomeMae: null,
+  email: null,
+  enderecoPrincipal: {
+    tipo: null,
+    zonaLocalizacao: null,
+    semNumero: null,
+    logradouro: null,
+    numero: null,
+    bairro: null,
+    complemento: null,
+    cep: null,
+    uf: null,
+    municipio: null,
+    descricaoAcesso: null
+  },
+  enderecoCorrespondencia: {
+    tipo: null,
+    zonaLocalizacao: null,
+    semNumero: null,
+    logradouro: null,
+    numero: null,
+    bairro: null,
+    complemento: null,
+    cep: null,
+    uf: null,
+    municipio: null,
+    descricaoAcesso: null
+  }
+};
