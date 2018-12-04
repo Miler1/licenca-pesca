a645241aa4ec55d653a756c78dcdec5a65fad419
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-04 18:19:07 -0200

    #18 Reestruturando validators

---
+++ frontend/src/utils/validations/pessoa_rules.js
@@ -0,0 +1,69 @@
+import { required, validate } from "./utils";
+import {
+  checkCPF,
+  checkDate,
+  checkEmail,
+  checkNumero,
+  checkSemNumero,
+  checkConfirmEmail
+} from "./pessoa_checks";
+
+export const PESSOA_RULES = {
+  nome: [required("nome")],
+
+  cpf: [required("cpf"), validate(checkCPF, "cpf.invalid")],
+
+  passaporte: [required("passaporte")],
+
+  dataNascimento: [
+    required("dataNascimento", "change"),
+    validate(checkDate, "dataNascimento.past", "change")
+  ],
+
+  sexo: [required("sexo", "change")],
+
+  nomeMae: [required("nomeMae")],
+
+  email: [required("email"), validate(checkEmail, "email.invalid")],
+
+  confirmarEmail: [
+    required("confirmarEmail"),
+    validate(checkConfirmEmail, "confirmarEmail.check")
+  ],
+
+  enderecoPrincipal: {
+    zonaLocalizacao: [required("zonaLocalizacao")],
+
+    logradouro: [required("logradouro")],
+
+    numero: [validate(checkNumero, "numero.vazio")],
+
+    semNumero: [validate(checkSemNumero, "numero.vazio", "change")],
+
+    bairro: [required("bairro")],
+
+    cep: [required("cep")],
+
+    municipio: [required("municipio")],
+
+    uf: [required("uf")],
+
+    descricaoAcesso: [required("descricaoAcesso")],
+  },
+
+  enderecoCorrespondencia: {
+    logradouro: [required("logradouro")],
+
+    numero: [validate(checkNumero, "numero.vazio")],
+
+    semNumero: [validate(checkSemNumero, "numero.vazio", "change")],
+
+    bairro: [required("bairro")],
+
+    cep: [required("cep")],
+
+    municipio: [required("municipio")],
+
+    uf: [required("uf")]
+  }
+};
