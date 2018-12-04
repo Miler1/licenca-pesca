4994c908827166a1948a1ce3b0e0211e2362379d
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-04 18:21:23 -0200

    #18 Constante de dados para select

---
+++ frontend/src/utils/layout/selectOptions.js
@@ -0,0 +1,19 @@
+import { SexoDTO } from "../../model/PessoaDTO";
+
+export const GENERO_OPTIONS = [
+  {
+    key: SexoDTO.masculino,
+    value: SexoDTO.masculino,
+    label: "masculino"
+  },
+  {
+    key: SexoDTO.feminino,
+    value: SexoDTO.feminino,
+    label: "feminino"
+  },
+  {
+    key: SexoDTO.outros,
+    value: SexoDTO.outros,
+    label: "outros"
+  }
+];
