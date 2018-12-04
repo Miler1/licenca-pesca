be34e7aae64fb403d5e98ff636d9df282d776ff1
Highlander Vilela Paiva Silva <highlanderpaiva.lemaf@gmail.com>
2018-12-04 18:24:08 -0200

    #18 Adicionando i18n para os componentes do element

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,11 +8,10 @@ import ApiService from "./services/shared/ApiService";
 import messages from "./utils/messages/messages";
 
 // 3th Imports
-import ElementUI from "element-ui";
-import locale from "element-ui/lib/locale/lang/pt-br";
-import Vue2Filters from "vue2-filters";
 import VueI18n from "vue-i18n";
+import ElementUI from "element-ui";
 import VueTheMask from "vue-the-mask";
+import Vue2Filters from "vue2-filters";
 
 // CSS Imports
 import "./assets/linearicons/style.css";
@@ -20,17 +19,21 @@ import "mdi/scss/materialdesignicons.scss";
 import "@/theme/main.sass";
 
 // Vue Inits & Configs
-Vue.prototype.$bus = new Vue();
 Vue.config.productionTip = false;
-Vue.use(ElementUI, { locale });
 Vue.use(Vue2Filters);
 Vue.use(VueI18n);
 Vue.use(VueTheMask);
+
 ApiService.init();
 const i18n = new VueI18n({
   locale: "PT-BR",
   messages
 });
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+});
+
+Vue.prototype.$bus = new Vue();
 Vue.prototype.$translator = i18n;
 
 // Instância Vue
