ef1a76f0dbd7e99325fba0cc056bc9ff22ba49a0
Fernanda Possato <fernanda.possato@lemaf.ufla.br>
2018-12-11 16:11:24 -0200

    Correção grant

--- backend/src/main/resources/db/migration/V1__inicial.sql
+++ backend/src/main/resources/db/migration/V1__inicial.sql
@@ -1,5 +1,6 @@
 CREATE SCHEMA carteira_pesca;
 
 GRANT ALL ON SCHEMA carteira_pesca TO postgres;
-GRANT USAGE ON SCHEMA carteira_pesca TO central_colosso;
+GRANT USAGE ON SCHEMA carteira_pesca TO carteira_pesca;
 ALTER SCHEMA carteira_pesca OWNER TO postgres;
+
