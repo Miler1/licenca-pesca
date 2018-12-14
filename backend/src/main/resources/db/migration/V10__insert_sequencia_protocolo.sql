6b118553a249c2ed07b00eef444ea751e773a516
Matheus <matheus.souza.lemaf@gmail.com>
2018-12-14 15:59:00 -0200

    Inserts da tabela sequencia_protocolo.

--- backend/src/main/resources/db/migration/V10__insert_sequencia_protocolo.sql
+++ backend/src/main/resources/db/migration/V10__insert_sequencia_protocolo.sql
@@ -1,3 +1,3 @@
-INSERT INTO carteira_pesca.sequencia_protocolo VALUE
+INSERT INTO carteira_pesca.sequencia_protocolo VALUES
 (0, 'sequencia_esportiva', 0, 0, 2018),
 (1, 'sequencia_esportiva', 0, 1, 2018);
