7bcf3e3fa737bcd7a861efc11761e3b931727d90
Joyce <joycemelo.lemaf@gmail.com>
2019-01-04 08:56:14 -0200

    tentando resolver problema no teste de whitelabel

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/CarteiraPescaApplication.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/CarteiraPescaApplication.java
@@ -1,7 +1,9 @@
 package br.ufla.lemaf.ti.carteirapesca;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,9 +11,11 @@ import org.springframework.context.annotation.Configuration;
  *
  * @author Highlander Paiva
  */
-@Configuration
-@SpringBootApplication(scanBasePackages={"br.ufla.lemaf.ti.carteirapesca"})
-@SuppressWarnings("CheckStyle")
+//@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"br.ufla.lemaf.ti.carteirapesca"})
+//@SpringBootApplication
+//@SuppressWarnings("CheckStyle")
 public class CarteiraPescaApplication {
 
 	/**
