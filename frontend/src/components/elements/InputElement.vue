0d32f1947894f5f67ecd3ce7c6905f1f2b69b399
Gustavo Lopes Dominguete <gustavolopes.lemaf@gmail.com>
2019-01-10 17:26:10 -0200

    Correção problema 342

--- frontend/src/components/elements/InputElement.vue
+++ frontend/src/components/elements/InputElement.vue
@@ -337,4 +337,18 @@ export default {
   margin-left: 4px;
 .el-form-item__label:before 
   content: '' !important;
+
+.el-button--primary
+
+  background-color: #007EFE !important;
+  border-color: #007EFE !important;
+
+  &:hover
+    background-color: #409EFF !important;
+    border-color: #409EFF !important;
+
+.el-button--primary.is-disabled
+  background-color: #409EFF!important;
+  border-color: #409EFF !important;
+  color: white !important;
 </style>
