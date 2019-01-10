1253db4f431568327f247ecc488fda821ea5e1ef
Gustavo Lopes Dominguete <gustavolopes.lemaf@gmail.com>
2019-01-10 15:35:13 -0200

    Remoção de comentarios e de bibliotecas nao utilizadas

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/interfaces/consulta/web/ConsultaController.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/interfaces/consulta/web/ConsultaController.java
@@ -131,16 +131,12 @@ public class ConsultaController {
 			var licenca = consultaApplication.consulta(protocoloObj);
 			var solicitante = licenca.getSolicitante();
 			var pessoa = registroApplication.buscarDadosSolicitante(solicitante);
-//			var caminhoCarteira = carteiraBuilder.gerarCarteira(protocoloObj,licenca.modalidade(), pessoa);
-//			var carteira = new File(caminhoCarteira);
 			var carteira = facade.gerarCarteira(protocoloObj, licenca, pessoa);
 
 			var httpHeaders = new HttpHeaders();
 			httpHeaders.setContentType(MediaType.APPLICATION_PDF);
 
 			var isr = new InputStreamResource(new FileInputStream(carteira));
-
-
 
 			return new ResponseEntity<InputStreamResource>(isr, httpHeaders, HttpStatus.OK);
 
