5dfef97bdf9de585bc3eb5a3958c07d1e020467b
Gustavo Lopes Dominguete <gustavolopes.lemaf@gmail.com>
2019-01-14 16:09:20 -0200

    Correção do cron e migrations

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/interfaces/jobs/AtualizacaoVencimento.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/interfaces/jobs/AtualizacaoVencimento.java
@@ -15,7 +15,7 @@ public class AtualizacaoVencimento {
 
 	private static final Logger log = LoggerFactory.getLogger(AtualizacaoVencimento.class);
 
-	@Scheduled(cron = "0 0 1/1 * ? *")
+	@Scheduled(cron = " 0 0 0 1/1 * ?")
 	public void atualizacaoAutomatica() throws Exception {
 		log.info("Atualização automática");
 		registroServiceFacade.atualizarCondicaoVencimento();
