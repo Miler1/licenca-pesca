e62248aca3a2049e5d7d346ce41a0cfc51333dff
Joyce <joycemelo.lemaf@gmail.com>
2019-01-14 15:33:12 -0200

    alteração nas rotas para erro

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/infrastructure/config/WebApplicationConfig.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/infrastructure/config/WebApplicationConfig.java
@@ -1,6 +1,11 @@
 package br.ufla.lemaf.ti.carteirapesca.infrastructure.config;
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,12 +18,13 @@ public class WebApplicationConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/notFound").setViewName("forward:/index.html");
 	}
 
-//	@Bean
-//	public WebServerFactoryCustomizer containerCustomizer() {
-//		return container -> {
-//			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
-//				"/notFound"));
-//		};
-//	}
+
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+		return container -> {
+			container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+				"/notFound"));
+		};
+	}
 
 }
