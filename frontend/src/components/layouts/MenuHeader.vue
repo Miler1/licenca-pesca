2d5c06dd2e256ddae99f3f37f091dcd706679e78
Gustavo Lopes Dominguete <gustavolopes.lemaf@gmail.com>
2019-01-31 17:39:31 -0200

    Correção problema de limpar tela inicial

--- frontend/src/components/layouts/MenuHeader.vue
+++ frontend/src/components/layouts/MenuHeader.vue
@@ -30,8 +30,8 @@ export default {
 	},
 	goHome() {
 		this.$store.dispatch(CANCELAR).then(p => {
-			this.step = 0;
-			this.$router.push({name: 'home'});
+			// this.$router.push({name: 'home'});
+			window.location = "/";
 		});
 	}
   }
