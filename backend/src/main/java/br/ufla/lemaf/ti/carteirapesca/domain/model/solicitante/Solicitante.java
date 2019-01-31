d0ac998d0fd4a3e504e99736167b8228fc95847d
Gustavo Lopes Dominguete <gustavolopes.lemaf@gmail.com>
2019-01-31 14:48:00 -0200

    Fix - Verificação de carteira ativa

--- backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/domain/model/solicitante/Solicitante.java
+++ backend/src/main/java/br/ufla/lemaf/ti/carteirapesca/domain/model/solicitante/Solicitante.java
@@ -95,8 +95,8 @@ public class Solicitante implements Entity<Solicitante, SolicitanteId> {
 	public boolean pussuiLicencaAtiva(Modalidade modalidade) {
 		return licenca
 			.stream()
-			.anyMatch(licencaProcurada -> (licencaProcurada.getStatus().getId().equals(Status.StatusEnum.ATIVO.id)) ||
-				licencaProcurada.getStatus().getId().equals(Status.StatusEnum.AGUARDANDO_PAGAMENTO_BOLETO.id)
+			.anyMatch(licencaProcurada -> ((licencaProcurada.getStatus().getId().equals(Status.StatusEnum.ATIVO.id)) ||
+				licencaProcurada.getStatus().getId().equals(Status.StatusEnum.AGUARDANDO_PAGAMENTO_BOLETO.id))
 				&& licencaProcurada.modalidade().getId().equals(modalidade.getId()));
 	}
 
