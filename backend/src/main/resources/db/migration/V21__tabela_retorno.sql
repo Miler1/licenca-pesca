5cf1a89333d574fb21c4a278ba78a5bacc8fff56
Matheus <matheus.souza.lemaf@gmail.com>
2019-02-11 14:57:32 -0200

    Criação da tabela retorno.

--- backend/src/main/resources/db/migration/V21__tabela_retorno.sql
+++ backend/src/main/resources/db/migration/V21__tabela_retorno.sql
@@ -2,30 +2,28 @@ CREATE TABLE carteira_pesca.retorno
 (
   id SERIAL NOT NULL,
   id_arquivo INTEGER NOT NULL,
-  dt_gravacao_banco DATE NULL,
-  dt_processamento TIMESTAMP NULL,
-  qtd_titulo_cobranca INTEGER NULL,
-  valor_titulo_cobranca DOUBLE PRECISION NULL,
-  qtd_confirmacao_entrada INTEGER NULL,
-  valor_confirmacao_entrada DOUBLE PRECISION NULL,
-  qtd_liquidacao INTEGER NULL,
-  valor_liquidacao DOUBLE PRECISION NULL,
-  qtd_baixados INTEGER NULL,
-  valor_baixados DOUBLE PRECISION NULL,
-  qtd_abatimento_cancelamento INTEGER NULL,
-  valor_abatimento_cancelamento DOUBLE PRECISION NULL,
-  qtd_vencimento_alterado INTEGER NULL,
-  valor_vencimento_alterado DOUBLE PRECISION NULL,
-  qtd_abatimento_concedido INTEGER NULL,
-  valor_abatimento_concedido DOUBLE PRECISION NULL,
-  qtd_confirmacao_instrucao_protesto INTEGER NULL,
-  valor_confirmacao_instrucao_protesto DOUBLE PRECISION NULL,
-  qtd_total_rateio_efetuado INTEGER NULL,
-  valor_total_rateio_efetuado DOUBLE PRECISION NULL,
-  numero_aviso_bancario INTEGER NULL,
-
+  dt_gravacao_banco DATE,
+  dt_processamento TIMESTAMP,
+  qtd_titulo_cobranca INTEGER,
+  valor_titulo_cobranca DOUBLE PRECISION,
+  qtd_confirmacao_entrada INTEGER,
+  valor_confirmacao_entrada DOUBLE PRECISION,
+  qtd_liquidacao INTEGER,
+  valor_liquidacao DOUBLE PRECISION,
+  qtd_baixados INTEGER,
+  valor_baixados DOUBLE PRECISION,
+  qtd_abatimento_cancelamento INTEGER,
+  valor_abatimento_cancelamento DOUBLE PRECISION,
+  qtd_vencimento_alterado INTEGER,
+  valor_vencimento_alterado DOUBLE PRECISION,
+  qtd_abatimento_concedido INTEGER,
+  valor_abatimento_concedido DOUBLE PRECISION,
+  qtd_confirmacao_instrucao_protesto INTEGER,
+  valor_confirmacao_instrucao_protesto DOUBLE PRECISION,
+  qtd_total_rateio_efetuado INTEGER,
+  valor_total_rateio_efetuado DOUBLE PRECISION,
+  numero_aviso_bancario INTEGER,
   CONSTRAINT pk_retorno PRIMARY KEY(id),
-
   CONSTRAINT fk_retorno_arquivo FOREIGN KEY (id_arquivo)
     REFERENCES carteira_pesca.arquivo (id) MATCH SIMPLE
     ON UPDATE RESTRICT ON DELETE RESTRICT
@@ -36,7 +34,6 @@ CREATE TABLE carteira_pesca.retorno
 ALTER TABLE carteira_pesca.retorno OWNER TO postgres;
 GRANT ALL ON TABLE carteira_pesca.retorno TO postgres;
 GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE carteira_pesca.retorno TO carteira_pesca;
-
 COMMENT ON TABLE carteira_pesca.retorno IS 'Entidade responsável por armazenar os dados do retorno.';
 COMMENT ON COLUMN carteira_pesca.retorno.id IS 'Identificador único da entidade.';
 COMMENT ON COLUMN carteira_pesca.retorno.id_arquivo IS 'Arquivo de retorno que foi gerado.';
@@ -64,10 +61,10 @@ COMMENT ON COLUMN carteira_pesca.retorno.valor_total_rateio_efetuado IS 'Número
 
 ALTER TABLE carteira_pesca.titulo DROP COLUMN dt_geracao_remessa;
 
-ALTER TABLE carteira_pesca.titulo ADD COLUMN valor_pago DOUBLE PRECISION NULL;
+ALTER TABLE carteira_pesca.titulo ADD COLUMN valor_pago DOUBLE PRECISION;
 COMMENT ON COLUMN carteira_pesca.titulo.valor_pago IS 'Valor que foi pago pelo titulo título.';
 
-ALTER TABLE carteira_pesca.titulo ADD COLUMN id_remessa INTEGER NULL;
+ALTER TABLE carteira_pesca.titulo ADD COLUMN id_remessa INTEGER;
 ALTER TABLE carteira_pesca.titulo ADD CONSTRAINT fk_remessa_titulo FOREIGN KEY (id_remessa)
   REFERENCES carteira_pesca.remessa (id) MATCH SIMPLE
   ON UPDATE RESTRICT ON DELETE RESTRICT;
@@ -82,5 +79,6 @@ ALTER TABLE carteira_pesca.titulo ADD CONSTRAINT fk_retorno_titulo FOREIGN KEY (
 COMMENT ON COLUMN carteira_pesca.titulo.id_retorno IS 'Retorno que o titulo foi processado.';
 
 ALTER TABLE carteira_pesca.remessa DROP COLUMN sequencia_nome_arquivo;
-ALTER TABLE carteira_pesca.remessa ADD COLUMN sequencia_nome_arquivo INTEGER NULL;
+ALTER TABLE carteira_pesca.remessa ADD COLUMN sequencia_nome_arquivo INTEGER;
 
+COMMENT ON COLUMN carteira_pesca.remessa.sequencia_nome_arquivo IS 'Sequência com nome do arquivo.';
