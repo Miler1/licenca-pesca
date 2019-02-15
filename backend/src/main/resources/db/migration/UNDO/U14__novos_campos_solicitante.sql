4b47495bd3ed6e84e928eed8af6301b07606b0ea
Matheus <matheus.souza.lemaf@gmail.com>
2019-02-15 11:48:39 -0200

    Adição do respectivo down da migration.

---
+++ backend/src/main/resources/db/migration/UNDO/U14__novos_campos_solicitante.sql
@@ -0,0 +1,3 @@
+ALTER TABLE carteira_pesca.solicitante DROP COLUMN numero_tentativas;
+ALTER TABLE carteira_pesca.solicitante DROP COLUMN data_ultima_tentativa;
+ALTER TABLE carteira_pesca.solicitante DROP COLUMN data_desbloqueio;
