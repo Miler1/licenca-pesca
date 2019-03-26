2490e308116cab40f6f9d49b58acf9a629341e29
Gabriel Oliveira <gabriel.oliveira.lemaf@gmail.com>
2019-03-26 09:13:19 -0300

    [sprint2] Validando evolution V30 com alteracao na tabela convenio

--- backend/src/main/resources/db/migration/v30__alteracao_tabela_convenio.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE carteira_pesca.convenio ADD COLUMN linha_digitavel CHARACTER VARYING(75) NOT NULL;
-COMMENT ON COLUMN carteira_pesca.convenio.linha_digitavel IS 'Conteúdo da linha digitável do boleto.';
-
-ALTER TABLE carteira_pesca.convenio RENAME COLUMN codigo_barra TO codigo_barras;
-COMMENT ON COLUMN carteira_pesca.convenio.codigo_barras IS 'Conteúdo do código de barras do boleto.';
