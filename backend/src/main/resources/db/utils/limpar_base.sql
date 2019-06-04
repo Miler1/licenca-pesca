6c72da7ec2bac5757b8fc4aad6441abbca0b13f9
Hiago Souza <hmarques.lemaf@gmail.com>
2019-06-04 18:26:44 -0300

    [:construction] Scripts para limpar base

---
+++ backend/src/main/resources/db/utils/limpar_base.sql
@@ -0,0 +1,6 @@
+TRUNCATE carteira_pesca.arquivo;
+TRUNCATE carteira_pesca.taxa_licenca;
+TRUNCATE carteira_pesca.licenca CASCADE;
+TRUNCATE carteira_pesca.solicitante CASCADE;
+TRUNCATE carteira_pesca.endereco_estrangeiro CASCADE;
+TRUNCATE carteira_pesca.endereco CASCADE;
